605cea24e7691d704935158be5ab444e5770eb12
Martmists <martmists@gmail.com>
2024-07-20 02:38:55 +0200

    bump version
    Signed-off-by: Martmists <martmists@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.martmists.ndarray-simd"
-version = "1.0.5"
+version = "1.0.6"
 val isProduction = (findProperty("production") ?: System.getProperty("production")) != null
 
 repositories {
