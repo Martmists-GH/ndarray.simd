330bb2985d3a1710849d8a7a638322e38c6896f0
Martmists <martmists@gmail.com>
2024-07-22 11:43:50 +0200

    doc for utils
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/commonMain/kotlin/com/martmists/ndarray/simd/impl/util.kt
+++ src/commonMain/kotlin/com/martmists/ndarray/simd/impl/util.kt
@@ -1,9 +1,23 @@
 package com.martmists.ndarray.simd.impl
 
+/**
+ * Checks if the index is in bounds.
+ *
+ * @param label The label of the index.
+ * @param pos The index.
+ * @param size The size of the array.
+ * @throws IndexOutOfBoundsException If the index is out of bounds.
+ */
 inline fun checkIndex(label: String, pos: Int, size: Int) {
     if (pos < 0 || pos >= size) {
         throw IndexOutOfBoundsException("$label must be in [0, $size), but was $pos")
     }
 }
 
+/**
+ * Returns the product of all elements in the array.
+ *
+ * @receiver The array.
+ * @return The product of all elements.
+ */
 inline fun IntArray.product() = fold(1, Int::times)
