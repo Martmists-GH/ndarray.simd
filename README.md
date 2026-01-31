4d5cafe0774e3053aae51c0fdbd205e34f7f4d94
Martmists <martmists@gmail.com>
2026-01-31 03:01:06 +0100

    bump version

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.martmists.ndarray-simd:ndarray-simd:1.7.5")
+    implementation("com.martmists.ndarray-simd:ndarray-simd:1.7.6")
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "com.martmists.ndarray-simd"
-version = "1.7.5"
+version = "1.7.6"
 val isProduction = (findProperty("production") ?: System.getProperty("production")) != null
 
 repositories {
